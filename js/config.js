7bd452de3c1cdb56dfab131784c901433d1e09c9
israelreyes-gif <israel.reyes@gmail.com>
2026-08-08 22:04:59 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 export const API_BASE_URL = 'https://faro-worker.israel-reyes.workers.dev';
 
-export const VAPID_PUBLIC_KEY = 'BEQUmNmv4W3RvkDJ7lYMJSpJ9CO2L0jZZA4salWO4VIvHb8egTkJCg2MgDYVVEhrLDYxuqY4-A9I04M7UhR9aH4';
+export const VAPID_PUBLIC_KEY = 'BE0LX5qBr311Q4VYzCT0kYmiRDpbk0UwEZsAYXnNr0IcVJwy_1wbCEtK9QDvrdeyNaA45raQcY7yoE9Lq8szXC4';
