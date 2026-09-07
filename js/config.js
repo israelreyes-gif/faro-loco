37f773b6e56ff2e371030418d95b74cb5e254f45
israelreyes-gif <israel.reyes@gmail.com>
2026-09-07 18:43:07 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
-export const API_BASE_URL = 'https://faro-loco-worker.israel-reyes.workers.dev';
+export const API_BASE_URL = 'https://api-proxy.israelreyes-gif.deno.net/faro-loco';
 
 export const VAPID_PUBLIC_KEY = 'BINZOW5Yr4fwSlSziyr642QtRO8oDfr2xUtYWQqKO6SiIt0Rb0o2idqgEDsocmEKlICkU1c22RmeI2iFMvG1U1s';
